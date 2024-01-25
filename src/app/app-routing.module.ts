ac0051d5fd27d303f7c51a86886bf4b2d01ac7d2
felender7 <felender7@gmail.com>
2024-01-25 22:03:50 +0200

    added routing path on routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { RequestedBooksComponent } from './requested-books/requested-books.component';
 
 const routes: Routes = [
-  {path:"", redirectTo:"/home",pathMatch:"full"},
-  {path:"home", component:DashboardComponent},
-  {path:"login", component:LoginComponent}
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: DashboardComponent },
+  { path: "login", component: LoginComponent }, 
+  {path: "requested-books", component: RequestedBooksComponent}
 ];
 
 @NgModule({
